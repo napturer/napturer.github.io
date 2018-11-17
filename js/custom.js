4893a3061123dcb127033d8bbc38c6672035b5ed
napturer <45115373+napturer@users.noreply.github.com>
2018-11-17 12:49:18 -0800

    Add background image

--- js/custom.js
+++ js/custom.js
@@ -20,7 +20,8 @@
         slides: [
             { src: 'images/slide-image01.jpg' },
             { src: 'images/slide-image02.jpg' },
-            { src: 'images/slide-image03.jpg' }
+            { src: 'images/slide-image03.jpg' },
+            { src: 'images/slide-image04.jpg' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
