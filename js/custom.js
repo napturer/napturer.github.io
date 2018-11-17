004a10fc2b891bbef3ff6775090ff7dadace8a5d
napturer <45115373+napturer@users.noreply.github.com>
2018-11-17 12:38:32 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,8 @@
     $('body').vegas({
         slides: [
             { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
+            { src: 'images/slide-image02.jpg' },
+            { src: 'images/slide-image03.jpg' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
